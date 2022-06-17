2fc207f4245d8a476e93efb880b54c92906b4197
Georges El hajj <georgeselhajj46@gmail.com>
2022-06-17 13:38:58 +0300

    Structures creation

--- Hotel.cpp
+++ Hotel.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
-#include <fstream>
+#include <cstring>
 #include <string>
 using namespace std;
-int main(){
+//creating Date structure
+struct Date{
+    int day;
+    int month;
+    int year;
+};
+//creating Room structure
+struct Room{
+    int num;
+    string address;
+    string type;
+    double price;
+    string *options;
+    Date start_date;
+    Date end_date;
+};
+//creating Client structure
+struct Client{
+    int ID;
+    string firstName;
+    string lastName;
+    string password;
+    string address;
+    string tel;
+    Room *r;
+};
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
